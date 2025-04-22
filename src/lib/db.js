e1456955ad7c93615aeb5c6020bbdf7a1ce7e6e0
emmilly <kaytheemmilly@gmail.com>
2025-04-22 09:30:26 -0300

    Alterando ,

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,6 +3,6 @@ import { Pool } from 'pg';
 export default new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false
   },
 });
