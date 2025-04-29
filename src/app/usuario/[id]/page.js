18f23bffe3356e62f19dbd45b2917648779286e0
emmilly <kaytheemmilly@gmail.com>
2025-04-29 09:06:37 -0300

    colocando cargos com o id

---
+++ src/app/usuario/[id]/page.js
@@ -0,0 +1,13 @@
+import db from "@/lib/db"; // para acessar o banco de dados
+
+export default async ({params}) => { // dizendo que vai ser de forma assincrona, ou seja, lenta
+
+    const usuario = await db.query("select * from usuario where id = " + params.id); // fazendo consulta
+
+    return (        
+        <>
+            <h1> Pagina do aluno: {usuario.rows[0].nome} </h1>
+            <p>O aluno faz parte do projeto {usuario.rows[0].cargo} </p>
+        </>
+    )
+}
