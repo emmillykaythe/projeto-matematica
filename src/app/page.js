6708eb858fb776371dc555c4bba32548570b089b
emmilly <kaytheemmilly@gmail.com>
2025-05-06 07:55:06 -0300

    criação do perfil

--- src/app/page.js
+++ src/app/page.js
@@ -5,48 +5,13 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Isso aqui é um link <code>src/app/page.js</code>.
-          </li>
-          <li>Não sei para o que funciona ainda</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-           Aba 1
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Aba 2
-          </a>
+     
+      <div className={styles.circle}>
+          Perfil
         </div>
+        
       </main>
-      <footer className={styles.footer}>
+      <footer className={styles.footer}> //links
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
