d847388c817672f984eab4f23ffc4d4727b3d745
emmillykaythe <kaytheemmilly@gmail.com>
2025-05-11 21:13:43 -0300

    inserindo o acesso rapido

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function PaginaInicial() {
   return (
-  <div className={styles.page}>      
-    <div style={{ display: 'flex', justifyContent: "flex-end" }}>
-      <div className={styles.circle}> 
-        Perfil
+    <div className={styles.page}>
+      <div style={{ display: 'flex', justifyContent: "flex-end" }}>
+        <Link href="/usuario" className={styles.circle}>Perfil</Link>
+      </div>     
+      <div className={styles.abaLateral}></div>
+      <div className={styles.conteudo}> 
+        <h2 className={styles.tituloPrincipal}>ACESSO RÁPIDO</h2>
       </div>
-    </div>     
-        <div className= {styles.abaLateral}> </div>
-
-  </div>
+    </div>
   );
 }
+
+
+
+  
